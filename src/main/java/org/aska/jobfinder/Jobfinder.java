983f5bd70ee2d74e7108d8d8ea86327b719ad63b
Ansuz <adrianmrv02@gmail.com>
2022-06-07 17:26:15 +0200

    Initial commit

---
+++ src/main/java/org/aska/jobfinder/Jobfinder.java
@@ -0,0 +1,14 @@
+
+
+package org.aska.jobfinder;
+
+/**
+ *
+ * @author Adrian MRV. aka AMRV || Ansuz
+ */
+public class Jobfinder {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
