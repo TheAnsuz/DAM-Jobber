81791506a1cd9427154507e9c673a4424b451733
Sabri <saabb.tod@gmail.com>
2022-06-07 19:32:13 +0200

    Clases de user, userDAO y GlobalDAO

--- src/main/java/com/gf/dao/GlobalDAO.java
+++ src/main/java/com/gf/dao/GlobalDAO.java
@@ -1,9 +1,48 @@
 package com.gf.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 /**
  *
  * @author Adrian MRV. aka AMRV || Ansuz
  */
 public class GlobalDAO {
+
+    private static String url = "jdbc:mysql://localhost:3306/jobber";
+    private static Connection con;
+    public static Statement st;
+
+     public static void conectarBaseDatos(){
+        try {
+            con = DriverManager.getConnection(url, "root", "");
+            st = con.createStatement();
+        } catch (Exception e) {
+            System.out.println("Error");
+        }
+    }
+     
+     public static void conexion(){
+         if (con == null){
+             conectarBaseDatos();
+         } else {
+             System.out.println("Ya estas conectado a la base de datos");
+         }
+     }
+
+    public static String getUrl() {
+        return url;
+    }
+
+    public static Connection getCon() {
+        conexion();
+        return con;
+    }
+
+    public static Statement getSt() {
+        return st;
+    }
+    
     
 }
