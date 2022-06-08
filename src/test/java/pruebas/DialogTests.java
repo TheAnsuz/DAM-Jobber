929398f89477bc7eda10f0d180d8427a26b1be9c
AMRV <adrianmrv02@gmail.com>
2022-06-08 10:04:09 +0200

    Tests

---
+++ src/test/java/pruebas/DialogTests.java
@@ -0,0 +1,16 @@
+package pruebas;
+
+import com.gf.view.InfoDialog;
+
+/**
+ *
+ * @author Adrian MRV. aka AMRV || Ansuz (org.amrv)
+ */
+public class DialogTests {
+
+    public static void main(String[] args) {
+        InfoDialog dialog = new InfoDialog(null, false);
+        dialog.setText("Me cago en tu putisima madre hijo de remil putas", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo", "Autismo");
+        dialog.setVisible(true);
+    }
+}
